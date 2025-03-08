80db0b63639afe5e141443d7fd56c03e7aec300a
Ashutosh-Chaturvedi <ashutoshchaturvedi734@gmail.com>
2025-03-08 21:07:50 +0530

    NewAdmission

--- studentmanagement.cpp
+++ studentmanagement.cpp
@@ -2,6 +2,7 @@
 #include<string>
 #include<chrono>
 #include<thread>
+#include<vector>
 
 #include"color.h"
 
@@ -12,6 +13,17 @@ void printWithDelay(const std::string& text, const std::string& formats = RESET,
         std::this_thread::sleep_for(std::chrono::milliseconds(delay));
     }
     std::cout << RESET;
+}
+
+void progressBar() {
+    std::cout << "Processing: [" << BLUE;
+    
+    for (int i = 0; i < 20; i++) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << "#";
+        std::cout.flush(); 
+    }
+    std::cout << RESET << "] Done!\n";
 }
 
 template <typename T>
@@ -53,26 +65,39 @@ class grade {
     }
 };
 
-void newAdmission() {
-    
+void newAdmission(std::vector<student>& students) {
+    printWithDelay("\n\tNew Admission\n",BOLD_BLUE);
+    int rollno = getValidatedInput<int>("Enter roll number: ",true);
+    std::string name;
+    printWithDelay("Enter the name: ");
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::getline(std::cin, name);
+    int age = getValidatedInput<int>("Enter age: ", true);
+    char grade = getValidatedInput<char>("Enter grade: ", true);
+    student* s = new student(rollno,name,age,grade);
+    students.push_back(*s);
+    progressBar();
+    printWithDelay("\nSTUDENT DETAIL ADDED SUCCESSFULLY\n",BOLD_GREEN);
+    return;
 }
 
-void list() {
+void list(std::vector<student>& students) {
 
 }
 
-void menu() {
-    std::cout << BOLD_GREEN << "1. " << RESET << BOLD << "New Admission\n" << RESET;
-    std::cout << BOLD_GREEN << "2. " << RESET << BOLD << "List Students\n" << RESET;
-    std::cout << BOLD_RED << "0. " << RESET << BOLD << "Exit\n" << RESET;
+void menu(std::vector<student>& students) {
+    printWithDelay( "\nWelcome to the Student Management System\n", BOLD_BLUE);
+    printWithDelay("1. New Admission\n");
+    printWithDelay("2. List Students\n");
+    printWithDelay("0. Exit\n");
     int choice = getValidatedInput<int>("Choose from the above options: ", true, BOLD_GREEN);
     switch (choice)
     {
     case 1:
-        newAdmission();
+        newAdmission(students);
         break;
     case 2:
-        list();
+        list(students);
         break;
     case 0:
         return;
@@ -80,11 +105,11 @@ void menu() {
         std::cout << BOLD_RED << "Invalid option" << RESET;
         break;
     }
+    menu(students);
 }
 
 int main() {
-    printWithDelay( "\nWelcome to the Student Management System\n", BOLD_BLUE);
-    menu();
-    newAdmission();
-    list();
+    std::vector<student> students;
+    system("cls");
+    menu(students);
 }
